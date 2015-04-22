a375f8694993e1a44a58cf87cc3187cddabcc768
chrisstates <d78cc145@opayq.com>
2015-04-22 23:15:17 +0300

    work in progress

--- UART.c
+++ UART.c
@@ -1,9 +1,15 @@
 unsigned long temp;
-char uart_rd,tempF;
-unsigned short MS_Byte, LS_Byte;
+char uart_rd, uart_rd1,tempF;
+unsigned short MS_Byte, LS_Byte, MS_old, LS_old, avg_MS, avg_LS;
 signed short tempC;
+signed int tempOffset;
+char output[11];
+int i;
 
   void readIntTemp () {
+       // get old temps...
+       MS_old = MS_Byte;
+       LS_old = LS_Byte;
     // Configure FVR to 2.048 V for ADC
     FVRCON = 0b11000010 ;
     TSEN_bit=1;      //temp sensor enable
@@ -22,13 +28,15 @@ signed short tempC;
     ADCON0.CHS3 = 1;     //1
     ADCON0.CHS4 = 1;     //1
     ADCON0.ADON = 1;  // enable A/D converter
-    //ADC_Init();
+
     ADCON0.F1 = 1;     // start conversion, GO/DONE = 1
     while (ADCON0.F1); // wait for conversion
     MS_Byte = ADRESH;
     LS_Byte = ADRESL;
-    temp = MS_Byte*256 + LS_Byte;
-    tempC = (2*temp - 400)/19.5-4;
+    avg_MS=(MS_Byte+MS_old)/2;
+    avg_LS=(LS_Byte+LS_old)/2;
+    temp = avg_MS*256 + avg_LS;
+    tempC = (2*temp - 400)/19.5+tempOffset;
     Delay_ms(200);
 
     TSEN_bit=0;
@@ -43,14 +51,35 @@ void main() {
   RXDTSEL_bit = 1;
   TXCKSEL_bit = 1;    //alternate pins RX=RA4, TX=RA5
 
-  UART1_Init(9600);                // Initialize UART module at 9600 bps
-  Delay_ms(100);                    // Wait for UART module to stabilize
-  
+  UART1_Init(9600);                  // Initialize UART module at 9600 bps
+  Delay_ms(100);                     // Wait for UART module to stabilize
+  UART1_Write_Text("Press m for menu");
+  ADC_Init();                        // init adc with default settings
+  TRISA2_bit=0;                      //*
+  RA2_bit=1;                         //* heartbeat init
+  tempOffset=  EEPROM_Read(0x00);    // temperature offset is in EEPROM at 0x00
+  if (tempOffset==0xFF)  {
+     EEPROM_Write(0x00,0xFD);        //init temp offset
+     tempOffset = 0xFD;
+  }
+
   while (1) {                     // Endless loop
+  i=0;
+    while(i<11){
+    output[i]=0x00;
+    i++;
+  }
     if (UART1_Data_Ready()) {     // If data is received,
-      uart_rd = UART1_Read();
-      UART1_Write(uart_rd);       // and send data via UART
+      UART1_Read_Text(output, 0x77, 11); //reads text until '\n' is found
+      /*uart_rd = UART1_Read();
+      if (uart_rd==0x6F)          //char 'o'--offset
+        while (UART1_Data_Ready()){
+           uart_rd1= UART1_Read();
+           EEPROM_Write(0x00,uart_rd1);
+      UART1_Write(uart_rd);       // and send data via UART*/
     }
+    UART1_Write_Text(output);
+    RA2_bit=~RA2_bit; //heartbeat
     readIntTemp();
     UART1_Write(MS_Byte);
     Delay_ms(50);
